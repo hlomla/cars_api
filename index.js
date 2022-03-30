c79b4daea37e93d04dd2849cea9e007427aae7c5
hlomla <hlomla.tapuko@gmail.com>
2022-03-30 10:58:09 +0200

    added list display all my car makes and the different colors available

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const carsElem = document.querySelector('.cars');
+const carColoursElem = document.querySelector('.colors');
+const carMakeElem = document.querySelector('.make')
 
 axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
     console.log(result.data);
@@ -11,6 +13,32 @@ axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
             <td>${car.make}</td>
         </tr>`
     carsElem.appendChild(li);
+
+    });
+})
+
+axios.get("https://api-tutor.herokuapp.com/v1/colors").then(function(result){
+    console.log(result.data);
+
+    result.data.forEach(color => {
+        const li = document.createElement('tr');
+        li.innerHTML = `<tr>
+            <td>${color}</td>
+        </tr>`
+        carColoursElem.appendChild(li);
+
+    });
+})
+
+axios.get("https://api-tutor.herokuapp.com/v1/makes").then(function(result){
+    console.log(result.data);
+
+    result.data.forEach(make => {
+        const li = document.createElement('tr');
+        li.innerHTML = `<tr>
+            <td>${make}</td>
+        </tr>`
+        carMakeElem.appendChild(li);
 
     });
 })
