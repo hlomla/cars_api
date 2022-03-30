67eb6c6e5f01b4142bfa552dce25b080b3b17c29
hlomla <hlomla.tapuko@gmail.com>
2022-03-30 16:35:59 +0200

    finished filtering widget

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const carsElem = document.querySelector('.cars');
 const carColoursElem = document.querySelector('.colors');
-const carMakeElem = document.querySelector('.make')
+const carMakeElem = document.querySelector('.make');
+const button = document.querySelector('.display')
+const displaySelected = document.querySelector('.carsFiltered')
 
 axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
-    console.log(result.data);
+    // console.log(result.data);
 
     result.data.forEach(car => {
         const li = document.createElement('tr');
@@ -17,28 +19,60 @@ axios.get("https://api-tutor.herokuapp.com/v1/cars").then(function(result){
     });
 })
 
-axios.get("https://api-tutor.herokuapp.com/v1/colors").then(function(result){
-    console.log(result.data);
 
-    result.data.forEach(color => {
-        const li = document.createElement('tr');
-        li.innerHTML = `<tr>
-            <td>${color}</td>
-        </tr>`
-        carColoursElem.appendChild(li);
 
-    });
+button.addEventListener('click', function(){
+    let selectedColor = document.querySelector('.filterColours').value
+    let selectedBrands = document.querySelector('.filterBrands').value;
+    console.log(selectedBrands);
+
+    if(selectedColor && selectedBrands){
+        axios.get(`https://api-tutor.herokuapp.com/v1/cars`).then(function(result){
+            console.log(result.data);
+        
+            result.data.forEach(car => {
+                if(car.color === selectedColor && car.make === selectedBrands){
+
+                    const li = document.createElement('tr');
+                    li.innerHTML = `<tr>
+                        <td>${car.color}</td>
+                        <td>${car.make}</td>
+                    </tr>`
+                    displaySelected.appendChild(li);
+                }
+                });
+        })
+ 
+    }
+    // // if(selectedColor){
+    //     axios.get(`https://api-tutor.herokuapp.com/v1/cars/color/${selectedColor}`).then(function(result){
+    //         // console.log(result.data);
+        
+    //         result.data.forEach(car => {
+    //             const li = document.createElement('tr');
+    //             li.innerHTML = `<tr>
+    //                 <td>${car.color}</td>
+    //                 <td>${car.make}</td>
+    //             </tr>`
+    //             displaySelected.appendChild(li);
+    //         });
+    //     })
+    // // }
+    // console.log(selectedBrands);
+    // if(selectedBrands){
+    //     axios.get(`https://api-tutor.herokuapp.com/v1/cars/make/${selectedBrands}`).then(function(result){
+    //         console.log(result.data);
+        
+    //         result.data.forEach(car => {
+    //             const li = document.createElement('tr');
+    //             li.innerHTML = `<tr>
+    //                 <td>${car.make}</td>
+    //             </tr>`
+    //             displaySelected.appendChild(li);
+    //         });
+    //     })
+    // }
 })
 
-axios.get("https://api-tutor.herokuapp.com/v1/makes").then(function(result){
-    console.log(result.data);
 
-    result.data.forEach(make => {
-        const li = document.createElement('tr');
-        li.innerHTML = `<tr>
-            <td>${make}</td>
-        </tr>`
-        carMakeElem.appendChild(li);
-
-    });
-})
+	
